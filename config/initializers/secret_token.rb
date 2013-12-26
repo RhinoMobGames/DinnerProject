d64610690fc3b261b67cc32c998f2e30490cc535
Erwin Felicilda <erwin@rhinomob.com>
2013-12-26 09:59:43 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dinner::Application.config.secret_key_base = 'a59f87e5761886981d1655681107ce38dc45f0f7b096ed07a02ea3ad97434a0fd12881903fc8dbccc7593d492d56c68bb07644b5ed984c8c80c352fd8b0cb8ec'
